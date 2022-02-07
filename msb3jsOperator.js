bb443e9c174d336c07cb23470ffaa1f8d720954b
ahkamul <sasraishougat@gmail.com>
2022-02-07 11:55:37 +0600

    msb3jsOperator.js created to write code on JS Operator

---
+++ msb3jsOperator.js
@@ -0,0 +1,101 @@
+// 3.1 Arithmetic Operator:
+
+// let num1 = 10;
+// let num2 =  2;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 ** num2);
+// console.log(num1 % num2);
+
+// console.log(40 % 2);
+// console.log(41 % 2);
+
+// 3.2 Assignment Operators
+
+// let num1 = 10;
+// let num2 = num1;
+
+// console.log (num1, num2);
+
+// num1 = num1 + 5;  num1 += 5;
+
+// num1 = num1 - 5;  num1 -= 5;
+
+// num1 = num1 * 5;  num1 *= 5;
+
+// num1 = num1 / 5;  num1 /= 5;
+
+// num1 = num1 ** 5;  num1 **= 5;
+
+// num1 = num1 % 5;  num1 %= 5;
+
+// num1 = num1 + 1; num1 += 1; num1++;
+// num1 = num1 - 1; num1 -= 1; num1--;
+
+
+// 3.3 Comparison Operator:
+
+//  > Greater than sign
+//  < Less than sign
+//  >= Greater equal sign
+//  <= Less equal sign
+//  === Equality sign
+//  !== Not equal sign 
+// i.e. We will get boolean result from Comparison Operator.
+
+// let price = 200;
+
+// console.log(price > 200);
+// console.log(price >= 100);
+// console.log(price < 50);
+// console.log(price <=500);
+// console.log(price === 100);
+// console.log(price !== 100);
+
+
+// 3.4 Logical Operator : 
+
+//    && (Logical and)
+//    || (Logical or)
+//    !  (Logical not)
+
+// let price = 10;
+
+// console.log (price > 5 && price < 15); // true, bcz both true
+// console.log (price > 50 && price < 15); // false, bcz one false
+
+// console.log (price > 50 || price < 15); // true, bcz anyone true
+
+// console.log (price > 5);   // true
+// console.log (!(price > 5)); // false, bcz it will reverse the real
+
+// 3.5 Logical Operator :
+
+// let price = 10;
+
+// console.log (price === 10);  // true
+// console.log (price === '10');  // false
+// console.log (price !== 20);  // true
+
+// console.log(price == 10); // true 
+// console.log(price == '10'); // true 
+
+// console.log(price != 20); // true
+
+
+// 3.6 Exercise - Swap two numbers:
+
+// let a = 10;
+// let b = 20;
+
+// let temp = a;
+
+// a = b;
+// b = temp;
+
+// console.log(a, b); 
+
+
