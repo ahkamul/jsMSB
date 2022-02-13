9c5e316e1c4e2a78356cdb71b52c8c30be8ebb5c
ahkamul <sasraishougat@gmail.com>
2022-02-13 16:17:49 +0600

    msb5jsLoops.js inited to write codes on JS Loop and 5.1 & 5.2 completed

---
+++ msb5jsLoops.js
@@ -0,0 +1,20 @@
+// 5.1 For Loop
+
+// for (let index = 1; index <= 5; index ++) {
+//     console.log ('Hello World', index);
+// }
+
+// for (let index = 5; index >= 1; index --) {
+//     console.log ('How are you', index)
+// }
+
+// 5.2 While loop 
+
+let index = 1;
+
+while (index <= 5){
+    console.log ('Hello World', index);
+    index ++;
+}
+
+// console.log (index);
